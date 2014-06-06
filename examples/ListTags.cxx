2c1fa5ac79158e90a6a23121223b9a650e804034
Kenneth Moreland <kmorel@sandia.gov>
2014-06-06 17:16:32 -0600

    Document ListForEach for template lists.
    This should finish off the basic provisions chapter (for now).

--- examples/ListTags.cxx
+++ examples/ListTags.cxx
@@ -22,9 +22,13 @@ struct MyCommonTypes :
 
 } // anonymous namespace
 
+#include <vtkm/VectorTraits.h>
+
 #include <vtkm/testing/Testing.h>
 
+#include <algorithm>
 #include <string>
+#include <vector>
 
 ////
 //// BEGIN-EXAMPLE BaseListTags.cxx
@@ -105,10 +109,76 @@ void TestCustomTypeLists()
   TryListTag(MyCommonTypes(), "Id2Vector2IdScalarVector3");
 }
 
+////
+//// BEGIN-EXAMPLE ListForEach.cxx
+////
+struct MyArrayBase {
+  // A virtual destructor makes sure C++ RTTI will be generated. It also helps
+  // ensure subclass destructors are called.
+  virtual ~MyArrayBase() {  }
+};
+
+template<typename T>
+struct MyArrayImpl : public MyArrayBase {
+  std::vector<T> Array;
+};
+
+template<typename T>
+void PrefixSum(std::vector<T> &array)
+{
+  T sum(typename vtkm::VectorTraits<T>::ComponentType(0));
+  for (typename std::vector<T>::iterator iter = array.begin();
+       iter != array.end();
+       iter++)
+  {
+    sum = sum + *iter;
+    *iter = sum;
+  }
+}
+
+struct PrefixSumFunctor {
+  MyArrayBase *ArrayPointer;
+
+  PrefixSumFunctor(MyArrayBase *arrayPointer) : ArrayPointer(arrayPointer) {  }
+
+  template<typename T>
+  void operator()(T) {
+    typedef MyArrayImpl<T> ConcreteArrayType;
+    ConcreteArrayType *concreteArray =
+        dynamic_cast<ConcreteArrayType *>(this->ArrayPointer);
+    if (concreteArray != NULL)
+    {
+      PrefixSum(concreteArray->Array);
+    }
+  }
+};
+
+void DoPrefixSum(MyArrayBase *array)
+{
+  PrefixSumFunctor functor = PrefixSumFunctor(array);
+  vtkm::ListForEach(functor, vtkm::TypeListTagCommon());
+}
+////
+//// END-EXAMPLE ListForEach.cxx
+////
+
+void TestPrefixSum()
+{
+  MyArrayImpl<vtkm::Id> array;
+  array.Array.resize(10);
+  std::fill(array.Array.begin(), array.Array.end(), 1);
+  DoPrefixSum(&array);
+  for (vtkm::Id index = 0; index < 10; index++)
+  {
+    VTKM_TEST_ASSERT(array.Array[index] = index+1, "Got bad prefix sum.");
+  }
+}
+
 void Test()
 {
   TestBaseListTags();
   TestCustomTypeLists();
+  TestPrefixSum();
 }
 
 } // anonymous namespace

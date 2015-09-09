fe93ee546e89bab6f23ab81215d53de51dd0eea0
Kenneth Moreland <kmorel@sandia.gov>
2015-09-09 14:18:37 -0600

    Add documentation for ExecutionWholeArray

--- examples/UseWorkletMapField.cxx
+++ examples/UseWorkletMapField.cxx
@@ -1,3 +1,5 @@
+#include <vtkm/exec/ExecutionWholeArray.h>
+
 ////
 //// BEGIN-EXAMPLE UseWorkletMapField.cxx
 ////
@@ -7,8 +9,7 @@
 #include <vtkm/cont/ArrayHandle.h>
 #include <vtkm/cont/DynamicArrayHandle.h>
 
-// TODO: Use VTK-m math when available.
-#include <math.h>
+#include <vtkm/VectorAnalysis.h>
 
 //// PAUSE-EXAMPLE
 namespace {
@@ -32,12 +33,10 @@ public:
   //// BEGIN-EXAMPLE WorkletOperator.cxx
   template<typename T, vtkm::IdComponent Size>
   VTKM_EXEC_EXPORT
-  vtkm::Float64 operator()(const vtkm::Vec<T,Size> &inVector) const
+  T operator()(const vtkm::Vec<T,Size> &inVector) const
   {
   //// END-EXAMPLE WorkletOperator.cxx
-    T magnitudeSquared = vtkm::dot(inVector, inVector);
-    // TODO: Use VTK-m math when available.
-    return sqrt(vtkm::Float64(magnitudeSquared));
+    return vtkm::Magnitude(inVector);
   }
 };
 
@@ -54,6 +53,52 @@ InvokeMagnitude(vtkm::cont::DynamicArrayHandle input)
 }
 ////
 //// END-EXAMPLE UseWorkletMapField.cxx
+////
+
+////
+//// BEGIN-EXAMPLE RandomArrayAccess.cxx
+////
+struct ReverseArrayCopy : vtkm::worklet::WorkletMapField
+{
+  typedef void ControlSignature(FieldIn<> inputArray,
+                                ExecObject outputArray);
+  typedef void ExecutionSignature(_1, _2, WorkIndex);
+  typedef _1 InputDomain;
+
+  template<typename InputType, typename OutputArrayPortalType>
+  VTKM_EXEC_EXPORT
+  void operator()(const InputType &inputValue,
+                  const OutputArrayPortalType &outputArrayPortal,
+                  vtkm::Id workIndex) const
+  {
+    vtkm::Id outIndex = outputArrayPortal.GetNumberOfValues() - workIndex - 1;
+    if (outIndex >= 0)
+    {
+      outputArrayPortal.Set(outIndex, inputValue);
+    }
+    else
+    {
+      this->RaiseError("Output array not sized correctly.");
+    }
+  }
+};
+
+template<typename T, typename Storage>
+VTKM_CONT_EXPORT
+vtkm::cont::ArrayHandle<T>
+InvokeReverseArrayCopy(const vtkm::cont::ArrayHandle<T,Storage> &inArray)
+{
+  vtkm::cont::ArrayHandle<T> outArray;
+  vtkm::exec::ExecutionWholeArray<T>
+      outArrayExecObject(outArray, inArray.GetNumberOfValues());
+
+  vtkm::worklet::DispatcherMapField<ReverseArrayCopy> dispatcher;
+  dispatcher.Invoke(inArray, outArrayExecObject);
+
+  return outArray;
+}
+////
+//// END-EXAMPLE RandomArrayAccess.cxx
 ////
 
 } // anonymous namespace
@@ -89,6 +134,17 @@ void Test()
     Vec3 testValue = TestValue(index, Vec3());
     vtkm::Float64 expectedValue = sqrt(vtkm::dot(testValue,testValue));
     vtkm::Float64 gotValue = outputArray.GetPortalConstControl().Get(index);
+    VTKM_TEST_ASSERT(test_equal(expectedValue, gotValue), "Got bad value.");
+  }
+
+  vtkm::cont::ArrayHandle<Vec3> outputArray2 =
+      InvokeReverseArrayCopy(inputArray);
+  VTKM_TEST_ASSERT(outputArray2.GetNumberOfValues() == ARRAY_SIZE,
+                   "Bad output array size.");
+  for (vtkm::Id index = 0; index < ARRAY_SIZE; index++)
+  {
+    Vec3 expectedValue = TestValue(ARRAY_SIZE - index - 1, Vec3());
+    Vec3 gotValue = outputArray2.GetPortalConstControl().Get(index);
     VTKM_TEST_ASSERT(test_equal(expectedValue, gotValue), "Got bad value.");
   }
 }

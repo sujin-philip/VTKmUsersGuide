f627c91e0a722031d9948b779aa5fcd33eea8b52
Kenneth Moreland <kmorel@sandia.gov>
2016-03-16 17:31:03 -0600

    Document the existing readers and writers.
    I am also making a change to the VTK-m code to make VTKDataSetWriter
    more consistent with VTKDataSetReader.

---
+++ examples/IO.cxx
@@ -0,0 +1,61 @@
+////
+//// BEGIN-EXAMPLE VTKDataSetWriter.cxx
+////
+#include <vtkm/io/writer/VTKDataSetWriter.h>
+
+void SaveDataAsVTKFile(vtkm::cont::DataSet data)
+{
+  vtkm::io::writer::VTKDataSetWriter writer("data.vtk");
+
+  writer.WriteDataSet(data);
+}
+////
+//// END-EXAMPLE VTKDataSetWriter.cxx
+////
+
+////
+//// BEGIN-EXAMPLE VTKDataSetReader.cxx
+////
+#include <vtkm/io/reader/VTKDataSetReader.h>
+
+vtkm::cont::DataSet OpenDataFromVTKFile()
+{
+  vtkm::io::reader::VTKDataSetReader reader("data.vtk");
+
+  return reader.ReadDataSet();
+}
+////
+//// END-EXAMPLE VTKDataSetReader.cxx
+////
+
+#include <vtkm/cont/testing/MakeTestDataSet.h>
+#include <vtkm/cont/testing/Testing.h>
+
+namespace {
+
+void TestIO()
+{
+  std::cout << "Writing data" << std::endl;
+  vtkm::cont::testing::MakeTestDataSet makeDataSet;
+  vtkm::cont::DataSet createdData = makeDataSet.Make3DExplicitDataSetCowNose();
+  SaveDataAsVTKFile(createdData);
+
+  std::cout << "Reading data" << std::endl;
+  vtkm::cont::DataSet readData = OpenDataFromVTKFile();
+
+  const vtkm::cont::CellSet &createdCellSet = createdData.GetCellSet().GetCellSet();
+  const vtkm::cont::CellSet &readCellSet = readData.GetCellSet().GetCellSet();
+  VTKM_TEST_ASSERT(
+        createdCellSet.GetNumberOfCells() == readCellSet.GetNumberOfCells(),
+        "Createded and read data do not match.");
+  VTKM_TEST_ASSERT(
+        createdCellSet.GetNumberOfPoints() == readCellSet.GetNumberOfPoints(),
+        "Createded and read data do not match.");
+}
+
+}
+
+int IO(int, char *[])
+{
+  return vtkm::cont::testing::Testing::Run(TestIO);
+}

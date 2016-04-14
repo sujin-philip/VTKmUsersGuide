b28219e9ea4d060ed95240b79af033a50bfb0483
Kenneth Moreland <kmorel@sandia.gov>
2016-04-13 20:29:50 -0600

    Move timers to its own chapter.
    Also update the example code to actually run a filter.

--- examples/Timer.cxx
+++ examples/Timer.cxx
@@ -1,31 +1,33 @@
 #include <vtkm/cont/ArrayHandle.h>
+#include <vtkm/cont/DataSet.h>
 #include <vtkm/cont/Timer.h>
 
+#include <vtkm/filter/PointElevation.h>
+
+#include <vtkm/cont/testing/MakeTestDataSet.h>
 #include <vtkm/cont/testing/Testing.h>
 
 namespace {
 
 void DoTiming()
 {
+  vtkm::cont::DataSet dataSet =
+      vtkm::cont::testing::MakeTestDataSet().Make2DUniformDataSet0();
   ////
   //// BEGIN-EXAMPLE Timer.cxx
   ////
-  vtkm::cont::ArrayHandle<vtkm::Float32> results;
-  //// PAUSE-EXAMPLE
-  // This just makes sure that results has something allocated so that you
-  // don't crash when getting the portal. Delete this once the invoke works,
-  // since that should do the right thing.
-  results.PrepareForOutput(10, VTKM_DEFAULT_DEVICE_ADAPTER_TAG());
-  //// RESUME-EXAMPLE
-//  vtkm::cont::DispatchMapField<vtkm::worklet::Elevation> dispatcher;
+  vtkm::filter::PointElevation elevationFilter;
 
   vtkm::cont::Timer<> timer;
 
-//  dispatcher.Invoke(grid.GetPointCoordinates(), results);
+  vtkm::filter::FieldResult fieldResult =
+      elevationFilter.Execute(dataSet, dataSet.GetCoordinateSystem());
 
-  // This call makes sure data is pulled back to the host in a host/device
+  // This code makes sure data is pulled back to the host in a host/device
   // architecture.
-  results.GetPortalConstControl();
+  vtkm::cont::ArrayHandle<vtkm::Float64> outArray;
+  fieldResult.FieldAs(outArray);
+  outArray.GetPortalConstControl();
 
   vtkm::Float64 elapsedTime = timer.GetElapsedTime();
 

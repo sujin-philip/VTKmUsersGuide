08940fde32ca8d397668697d87b0df88023f5a8a
Kenneth Moreland <kmorel@sandia.gov>
2016-10-22 20:46:15 -0400

    Add documentation for creating FieldCell

--- examples/UseWorkletMapPointToCell.cxx
+++ examples/UseWorkletMapPointToCell.cxx
@@ -60,21 +60,105 @@ void FindCellCenters(vtkm::cont::DataSet &dataSet)
 //// END-EXAMPLE UseWorkletMapPointToCell.cxx
 ////
 
+#include <vtkm/filter/FilterCell.h>
+
+////
+//// BEGIN-EXAMPLE UseFilterCell.cxx
+////
+namespace vtkm {
+namespace filter {
+
+class CellCenters : public vtkm::filter::FilterCell<CellCenters>
+{
+public:
+  VTKM_CONT_EXPORT
+  CellCenters();
+
+  template<typename ArrayHandleType, typename Policy, typename DeviceAdapter>
+  VTKM_CONT_EXPORT
+  vtkm::filter::ResultField
+  DoExecute(const vtkm::cont::DataSet &inDataSet,
+            const ArrayHandleType &inField,
+            const vtkm::filter::FieldMetadata &FieldMetadata,
+            vtkm::filter::PolicyBase<Policy>,
+            DeviceAdapter);
+};
+
+}
+} // namespace vtkm::filter
+////
+//// END-EXAMPLE UseFilterCell.cxx
+////
+
+////
+//// BEGIN-EXAMPLE FilterCellImpl.cxx
+////
+namespace vtkm {
+namespace filter {
+
+VTKM_CONT_EXPORT
+CellCenters::CellCenters()
+{
+  this->SetOutputFieldName("");
+}
+
+template<typename ArrayHandleType, typename Policy, typename DeviceAdapter>
+VTKM_CONT_EXPORT
+vtkm::filter::ResultField
+CellCenters::DoExecute(const vtkm::cont::DataSet &inDataSet,
+                       const ArrayHandleType &inField,
+                       const vtkm::filter::FieldMetadata &fieldMetadata,
+                       vtkm::filter::PolicyBase<Policy>,
+                       DeviceAdapter)
+{
+  VTKM_IS_ARRAY_HANDLE(ArrayHandleType);
+  VTKM_IS_DEVICE_ADAPTER_TAG(DeviceAdapter);
+
+  if (!fieldMetadata.IsPointField())
+  {
+    throw vtkm::cont::ErrorControlBadType(
+          "Cell Centers filter operates on point data.");
+  }
+
+  vtkm::cont::DynamicCellSet cellSet =
+      inDataSet.GetCellSet(this->GetActiveCellSetIndex());
+
+  using ValueType = typename ArrayHandleType::ValueType;
+  vtkm::cont::ArrayHandle<ValueType> outField;
+
+  vtkm::worklet::DispatcherMapTopology<vtkm::worklet::CellCenter, DeviceAdapter>
+      dispatcher;
+
+  dispatcher.Invoke(vtkm::filter::ApplyPolicy(cellSet, Policy()),
+                    inField,
+                    outField);
+
+  std::string outFieldName = this->GetOutputFieldName();
+  if (outFieldName == "")
+  {
+    outFieldName = fieldMetadata.GetName() + "_center";
+  }
+
+  return vtkm::filter::ResultField(inDataSet,
+                                   outField,
+                                   outFieldName,
+                                   vtkm::cont::Field::ASSOC_CELL_SET,
+                                   cellSet.GetName());
+}
+
+}
+} // namespace vtkm::filter
+////
+//// END-EXAMPLE FilterCellImpl.cxx
+////
+
 #include <vtkm/cont/testing/MakeTestDataSet.h>
 #include <vtkm/cont/testing/Testing.h>
 
 namespace {
 
-void Test()
+void CheckCellCenters(const vtkm::cont::DataSet &dataSet)
 {
-  vtkm::cont::testing::MakeTestDataSet makeTestDataSet;
-
-  std::cout << "Making test data set." << std::endl;
-  vtkm::cont::DataSet dataSet = makeTestDataSet.Make3DUniformDataSet0();
-
-  std::cout << "Finding cell centers." << std::endl;
-  FindCellCenters(dataSet);
-
   std::cout << "Checking cell centers." << std::endl;
   vtkm::cont::CellSetStructured<3> cellSet;
   dataSet.GetCellSet().CopyTo(cellSet);
@@ -108,6 +192,31 @@ void Test()
       }
     }
   }
+}
+
+void Test()
+{
+  vtkm::cont::testing::MakeTestDataSet makeTestDataSet;
+
+  std::cout << "Making test data set." << std::endl;
+  vtkm::cont::DataSet dataSet = makeTestDataSet.Make3DUniformDataSet0();
+
+  std::cout << "Finding cell centers directly." << std::endl;
+  FindCellCenters(dataSet);
+
+  CheckCellCenters(dataSet);
+
+  std::cout << "Making fresh test data set." << std::endl;
+  dataSet = makeTestDataSet.Make3DUniformDataSet0();
+
+  std::cout << "Finding cell centers with filter." << std::endl;
+  vtkm::filter::CellCenters cellCentersFilter;
+  cellCentersFilter.SetActiveCellSet(0);
+  cellCentersFilter.SetOutputFieldName("cell_center");
+  vtkm::filter::ResultField results =
+      cellCentersFilter.Execute(dataSet, dataSet.GetCoordinateSystem());
+
+  CheckCellCenters(results.GetDataSet());
 }
 
 } // anonymous namespace
